6b69e5f02c0ccccfcca5d5b7b566ab5639790ae8
Tendry Arivony <80846844+TendryArivony@users.noreply.github.com>
2024-03-19 15:57:09 +0300

    Update assignment

--- model/assignment.js
+++ model/assignment.js
@@ -3,13 +3,29 @@ let Schema = mongoose.Schema;
 const mongoosePaginate = require('mongoose-aggregate-paginate-v2');
 
 let AssignmentSchema = Schema({
-    id: Number,
-    dateDeRendu: Date,
-    nom: String,
-    rendu: Boolean
+    titre: String,
+    description: String,
+    matiere: String,
+    dateLimite: String,
+    groupe: [{
+        idGroupe: String,
+        nom: String
+    }],
+    lien: String,
+    rendu: [{
+        idEtudiant : String,
+        matricule: String,
+        auteur: String,
+        dateRendu: String,
+        file: String,
+        description: String,
+        note: String,
+        remarque: String
+    }]
 });
 
 AssignmentSchema.plugin(mongoosePaginate);
+
 
 // C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
 // assignment est le nom de la collection dans la base de données
