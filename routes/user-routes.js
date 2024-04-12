91480876e98ce95ec8c37c1244b15d6f9b9fdb09
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-12 12:24:22 +0300

    Login without password

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -15,7 +15,9 @@ async function login(req, res) {
             return res.status(401).json({ error: 'Invalid username or password' });
         }
         const token = jwt.sign({ userId: user._id, username: user.username }, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h' });
-        res.status(200).json({ message: 'Login successful', token, user });
+        // Exclure le champ 'password' de l'objet 'user' renvoyé dans la réponse
+        const { password: _, ...userWithoutPassword } = user.toObject();
+        res.status(200).json({ message: 'Login successful', token, user: userWithoutPassword });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
