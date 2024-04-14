a917ce548644d61f2e76389dc2195907fbc9a8ee
Tendry Rakotoarivony <tendryarivony@gmail.com>
2024-04-14 21:02:32 +0300

    For matiere

--- model/matiere.js
+++ model/matiere.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let MatiereSchema = Schema({
-    nom: String
+    nom: String,
+    photo: String, // Champ pour stocker la photo en base64
+    type: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 const Matiere = mongoose.model('matiere', MatiereSchema);
--- routes/matiere-routes.js
+++ routes/matiere-routes.js
@@ -1,31 +1,63 @@
-    let Matiere = require('../model/matiere');
+const Matiere = require('../model/matiere');
 
-
-    function getMatieres(req, res){
-        Matiere.find((err, matiere) => {
-            if(err){
-                res.send(err)
-            }
-    
-            res.send(matiere);
-        });
+async function createMatiere(req, res) {
+    try {
+        const matiereData = req.body;
+        const matiere = new Matiere(matiereData);
+        await matiere.save();
+        res.status(201).json({ message: 'Matiere created successfully' });
+    } catch (error) {
+        res.status(500).send(error);
     }
+}
 
-    function saveMatiere(req, res){
-        let matiere = new Matiere();
-        matiere.nom = req.body.nom;
-    
-        console.log("POST matiere reçu :");
-        console.log(matiere)
-    
-        matiere.save( (err) => {
-            if(err){
-                res.send('cant post matiere ', err);
-            }
-            res.json({ message: `${matiere.nom} saved!`})
-        })
+async function getMatiereById(req, res) {
+    try {
+        const matiereId = req.params.id;
+        const matiere = await Matiere.findById(matiereId);
+        if (!matiere) {
+            return res.status(404).json({ message: "Matiere not found" });
+        }
+        res.json(matiere);
+    } catch (error) {
+        res.status(500).send(error);
     }
-    
+}
 
-    module.exports = { getMatieres,saveMatiere };
+async function updateMatiere(req, res) {
+    try {
+        const matiereId = req.body._id;
+        const updatedMatiere = req.body;
+        const matiere = await Matiere.findByIdAndUpdate(matiereId, updatedMatiere, { new: true });
+        if (!matiere) {
+            return res.status(404).json({ message: "Matiere not found" });
+        }
+        res.json({ message: 'Matiere updated successfully' });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
 
+async function deleteMatiere(req, res) {
+    try {
+        const matiereId = req.params.id;
+        const deletedMatiere = await Matiere.findByIdAndRemove(matiereId);
+        if (!deletedMatiere) {
+            return res.status(404).json({ message: "Matiere not found" });
+        }
+        res.json({ message: 'Matiere deleted successfully' });
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+async function getMatieres(req, res) {
+    try {
+        const matieres = await Matiere.find();
+        res.json(matieres);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+module.exports = { createMatiere, getMatiereById, updateMatiere, deleteMatiere, getMatieres };
