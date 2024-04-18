5e464558bf38629f7097036cf0874e31217c5e90
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-04-18 09:44:57 +0300

    ajout message d'erreur quand le token manque

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,8 +4,7 @@ function authenticateToken(req, res, next) {
     console.log('check token');
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
-
+    if (token == null) { return res.status(401).send({"error":"Unauthorized: Token is missing."});}
     jwt.verify(token, 'RANDOM_TOKEN_SECRET', (err, user) => {
         if (err) return res.sendStatus(403);
         req.user = user;
@@ -14,3 +13,4 @@ function authenticateToken(req, res, next) {
 }
 
 module.exports = authenticateToken;
+    
